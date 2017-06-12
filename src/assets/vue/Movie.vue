32195477d410a5ef424cdcf45edb5f57214f8bc6
Fábio Theophilo <fabiotheophilo@gmail.com>
2017-06-12 15:46:54 -0300

    First commit

---
+++ src/assets/vue/Movie.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.movie-content
+  .movie-info
+    span.movie-title {{data.title}}
+    span.movie-director {{data.director}}
+  .movie-detail(v-if='detail')
+    span Besteira
+</template>
+
+<script>
+export default {
+  props: ['data'],
+  data () {
+    return {
+      detail: false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
---
+++ src/main.vue
@@ -0,0 +1,130 @@
+<template lang="pug">
+// App
+#app
+  // Statusbar
+  f7-statusbar
+  // Left Panel
+  f7-panel(left='', reveal='', layout='dark')
+    f7-view#left-panel-view(navbar-through='', :dynamic-navbar='true')
+      f7-navbar(title='Categorias')
+      f7-pages
+        f7-page
+          //- f7-block(inner='')
+          //-   p Left panel content goes here
+          //- f7-block-title Load page in panel
+          //- f7-list
+          //-   f7-list-item(link='/about/', title='About')
+          //-   f7-list-item(link='/form/', title='Form')
+          //- f7-block-title Load page in main view
+          f7-list
+            f7-list-item(v-for='category in dataset.categories', :title='category.title', link-view='#main-view', link-close-panel='')
+  // Main Views
+  f7-views
+    f7-view#main-view(navbar-through='', :dynamic-navbar='true', main='')
+      // Navbar
+      f7-navbar
+        f7-nav-left
+          f7-link(icon='icon-bars', open-panel='left')
+        //- f7-nav-center(sliding='') Lista de Filmes
+      // Pages
+      f7-pages
+        f7-page
+          // Comeca aqui...
+          //- f7-list
+          //-   f7-list-group(v-for='value in movies.values')
+          //-     f7-list-item(:title='value.subtitle', group-title='')
+          //-     f7-list-item(v-for='movie in value.movies', :title='movie.title')
+
+          //- f7-block-title Welcome to my App
+          f7-searchbar(cancel-link='Cancel', search-list='#search-list')
+          // This block will become visible when there is nothing found
+          f7-list.searchbar-not-found
+            f7-list-item(title='Nothing found')
+          // Search through this list
+          //- f7-list#search-list.searchbar-found(media-list='', virtual='', :virtual-items='movies.values[0].movies', :virtual-height='63', :virtual-search-all='searchAll')
+          f7-list#search-list.searchbar-found
+            f7-list-group(v-for='value in movies.values')
+              f7-list-item(:title='value.subtitle', group-title='')
+              f7-list-item(v-for='movie in value.movies')
+                movie(:data='movie')
+          //-   // Templte 7 Virtual List Item Template
+          //-   t7-template
+          //-     f7-list-item(media-item='', link='#', :title="'{{title}}'", :subtitle="'{{director}}<br />{{year}}' ")
+          //- f7-block-title Welcome to my App
+          //- f7-searchbar(cancel-link='Cancel', search-list='#search-list')
+          //- // This block will become visible when there is nothing found
+          //- f7-list.searchbar-not-found
+          //-   f7-list-item(title='Nothing found')
+          //- // Search through this list
+          //- f7-list#search-list.searchbar-found(media-list='', virtual='', :virtual-items='movies.values[0].movies', :virtual-height='63', :virtual-search-all='searchAll')
+          //-   // Templte 7 Virtual List Item Template
+          //-   t7-template
+          //-     f7-list-item(media-item='', link='#', :title="'{{title}}'", :subtitle="'{{director}}<br />{{year}}' ")
+</template>
+
+<script>
+import json from './assets/json/movies.json'
+import Movie from './assets/vue/Movie'
+
+export default {
+  components: {
+    Movie
+  },
+  mounted () {
+    console.log('My Json ', json)
+  },
+  data () {
+    return {
+      dataset: json,
+      movies: json.categories[0],
+    }
+  },
+  methods: {
+    searchAll (query) {
+      // var self = this;
+      // var found = [];
+      // for (var i = 0; i < self.items.length; i++) {
+      //     if (self.items[i].name.indexOf(query) >= 0 ||
+      //         self.items[i].email.indexOf(query) >= 0 ||
+      //         query.trim() === '') found.push(i);
+      // }
+      return null;
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+/*white = #fff*/
+caramel = #ffd9a6
+martini = #b3a2a5
+cloud = #cbcbcb
+estern_blue = #2e9cb1
+
+
+#search-list
+  margin-top: 5px
+
+.navbar
+  background-color: estern_blue
+  color: #fff
+  font-weight: bold
+
+.searchbar
+  background-color: estern_blue
+
+.list-group-title
+  background-color: cloud
+  color: martini
+
+.panel .panel-left .panel-reveal .layout-dark .active
+  .pages
+    .page
+      .page-content
+        background-color: estern_blue
+
+.list-block-group
+  li
+    /*background-color: martini
+    color: cloud*/
+</style>
